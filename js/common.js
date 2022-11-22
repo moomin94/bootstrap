42750526cf8d857300bd8e45a6ef602cb372a942
moomin94 <aktmd2529@gmail.com>
2022-11-22 15:43:50 +0900

    Update style

--- js/common.js
+++ js/common.js
@@ -4,7 +4,6 @@ $(function(){
 
   $(window).on('scroll', function(e){
 		let scrollTop = $(this).scrollTop();
-    console.log($('.section-3').offset().top, scrollTop);
     if($('.section-3').offset().top/2 < scrollTop && $('.section-4').offset().top > scrollTop){
       // video autoplay;
       $('#vid').get(0).play();
